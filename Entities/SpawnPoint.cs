634ffee2b52d0b997b98466ebc11392547520a0d
LozenChen <865466388@qq.com>
2023-05-10 17:14:04 +0200

    add facings of spawn point

--- Entities/SpawnPoint.cs
+++ Entities/SpawnPoint.cs
@@ -1,7 +1,9 @@
 using Celeste.Mod.TASHelper.Utils;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Monocle;
 using TAS.EverestInterop.Hitboxes;
+using static Celeste.Player;
 
 namespace Celeste.Mod.TASHelper.Entities;
 
@@ -24,19 +26,29 @@ public class SpawnPoint : Entity {
 
     public List<Vector2> spawns;
 
-    public Session session;
+    public Level level;
     public SpawnPoint(Level level) {
         Depth = 1;
-        session = level.Session;
-        spawns = session.LevelData.Spawns;
+        this.level = level;
+        spawns = level.Session.LevelData.Spawns;
+        Collider = new Hitbox(8f, 11f, -4f, -11f);
     }
     public override void Render() {
         if (DebugRendered && TasHelperSettings.UsingSpawnPoint) {
             // we show the actual respawn point, instead of closest spawn point
             // respawn point are set by level transition in general, and sometimes by triggers
-            Vector2 RespawnPoint = session.RespawnPoint.Value;
+            Vector2 RespawnPoint = level.Session.RespawnPoint.Value;
             foreach (Vector2 spawn in spawns) {
-                Maddy.Draw(spawn - new Vector2(16f,32f), Vector2.Zero,  Color.White * (0.1f * (spawn == RespawnPoint ? TasHelperSettings.CurrentSpawnPointOpacity : TasHelperSettings.OtherSpawnPointOpacity)));
+                Facings Facing = Facings.Right;
+                SpawnFacingTrigger spawnFacingTrigger = CollideFirst<SpawnFacingTrigger>(new Vector2((int)spawn.X, (int)spawn.Y));
+                if (spawnFacingTrigger != null) {
+                    Facing = spawnFacingTrigger.Facing;
+                }
+                else if ((int)spawn.X > (float)level.Bounds.Center.X) {
+                    Facing = Facings.Left;
+                }
+                // we assume the intro type is neither IntroTypes.None nor WakeUp
+                Maddy.Draw(spawn - new Vector2(16f,32f), Vector2.Zero,  Color.White * (0.1f * (spawn == RespawnPoint ? TasHelperSettings.CurrentSpawnPointOpacity : TasHelperSettings.OtherSpawnPointOpacity)),1f,0f, Facing == Facings.Left ?SpriteEffects.FlipHorizontally : SpriteEffects.None);
             }
         }
     }
@@ -44,4 +56,8 @@ public class SpawnPoint : Entity {
     // the first room in 6A is a bit tricky, it seems the vanilla somehow make maddy respawn at a point which is not a spawn point?
     // CelesteTAS has a patch FixCh6FirstRoomLoad about this to fix console load command
     // .... ok let's just ignore it
+
+    public override void DebugRender(Camera camera) {
+        // do nothing, never render its hitbox
+    }
 }
