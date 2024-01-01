c9b4dc690ea0d37defb21769bfb677d1a0344bb0
LozenChen <865466388@qq.com>
2024-01-01 20:16:38 +0800

    optimize WhatsNew: now versions are sorted to be descending, and update logs use word wrap algorithm instead of manually inserting newlines

--- Source/Module/WhatsNew.cs
+++ Source/Module/WhatsNew.cs
@@ -1,15 +1,17 @@
 //#define ShowAllLog
 using Celeste.Mod.TASHelper.Utils;
 using Microsoft.Xna.Framework;
+using System.Text;
+
 namespace Celeste.Mod.TASHelper.Module;
 
 public static class WhatsNew {
 
     public static bool NewUpdateLogExist = false;
 
-    public static bool ShouldShow => NewUpdateLogExist && TasHelperSettings.SubscribeWhatsNew && UpdateLogs.Keys.IsNotEmpty(); // in case i forget to write update logs lol
+    public static bool ShouldShow => NewUpdateLogExist && TasHelperSettings.SubscribeWhatsNew && UpdateLogs.IsNotEmpty(); // in case i forget to write update logs lol
 
-    public static Dictionary<string, List<string>> UpdateLogs = new();
+    public static List<Tuple<string, List<string>>> UpdateLogs = new();
 
     public static Version CurrentVersion => TASHelperModule.Instance.Metadata.Version;
 
@@ -41,7 +43,9 @@ public static class WhatsNew {
         AddLog("1.8.10", "\"More Options\" -> \"Scrollable Console History Log\"");
         AddLog("1.8.11", "\"More Options\" -> \"Better Invincibility\".");
         AddLog("1.8.12", "Wind Speed Renderer. Enable it in \"More Options\" -> \"Show Wind Speed\".", "Add the \"What's New!\" page. You can unsubscribe it in \"More Options\" -> \"Subscribe What's New!\"");
-        AddLog("1.8.13", "Now in Predictor, you can use (Dotted) Polygon Line instead of Hitbox per Frame\nto show your future track.\nEnable it in \"Predictor\" -> \"Other\" -> \"Timeline Finest Scale\" -> \"(Dotted) Polygon Line\"", "Simplified Triggers, which will hide unimportant triggers.", "Now Camera-Related Triggers have a different color.\nEnable it in \"Custom Colors Config\" -> \"Switches\" -> \"Camera-Related Triggers Color\"");
+        AddLog("1.8.13", "Now in Predictor, you can use (Dotted) Polygon Line instead of Hitbox per Frame to show your future track. Enable it in \"Predictor\" -> \"Other\" -> \"Timeline Finest Scale\" -> \"(Dotted) Polygon Line\"", "Simplified Triggers, which will hide unimportant triggers.", "Now Camera-Related Triggers have a different color.Enable it in \"Custom Colors Config\" -> \"Switches\" -> \"Camera-Related Triggers Color\"");
+
+        UpdateLogs.Sort((x,y) => new Version(y.Item1).CompareTo(new Version(x.Item1)));
     }
 
     private static Version LastVersion;
@@ -49,7 +53,7 @@ public static class WhatsNew {
     
     private static void AddLog(string version, List<string> updateLogs) {
         if (LastVersion < new Version(version)) {
-            UpdateLogs.Add(version, updateLogs);
+            UpdateLogs.Add(new Tuple<string, List<string>>(version, updateLogs));
         }
     }
     private static void AddLog(string version, string log1) {
@@ -71,9 +75,9 @@ public static class WhatsNew {
     }
     public static Menu.EaseInSubMenu CreateWhatsNewSubMenu(TextMenu menu) {
         return new Menu.EaseInSubMenu(Menu.TASHelperMenu.ToDialogText("Whats New"), false).Apply(subMenu => {
-            foreach (string version in UpdateLogs.Keys) {
-                subMenu.Add(new TextMenuExt.ButtonExt(version) { TextColor = Color.White, Scale = 0.7f * Vector2.One });
-                foreach (string updateLog in UpdateLogs[version]) {
+            foreach (Tuple<string, List<string>> tuple in UpdateLogs) {
+                subMenu.Add(new TextMenuExt.ButtonExt(tuple.Item1) { TextColor = Color.White, Scale = 0.7f * Vector2.One });
+                foreach (string updateLog in tuple.Item2) {
                     subMenu.Add(new LogItem(" - " + updateLog) { TextColor = Color.Gray, Scale = 0.6f * Vector2.One});
                 }
             }
@@ -86,12 +90,43 @@ public static class WhatsNew {
     private class LogItem : TextMenuExt.ButtonExt {
 
         public int lines = 1;
-        public LogItem(string label) : base(label) {
-            lines = 1 + label.Count(x => x == '\n');
+        public LogItem(string label, int width = 90) : base(string.Join("\n", label.Split('\n').Select(x => WordWrap(x, width)))) {
+            lines = 1 + this.Label.Count(x => x == '\n');
+        }
+
+        public static string WordWrap(string str, int width) {
+            string[] words = str.Split(' ').Select(x => x + " ").ToArray();
+            int curLineLength = 0;
+            StringBuilder sb = new();
+            for (int i = 0; i < words.Length; i++) {
+                string word = words[i];
+                if (curLineLength + word.Length > width) {
+                    if (curLineLength > 0) {
+                        sb.Append("\n");
+                        curLineLength = 0;
+                    }
+                    while (word.Length > width) {
+                        sb.Append(word.Substring(0, width - 1) + "-\n");
+                        word = word.Substring(width - 1);
+                    }
+                    word = word.TrimStart();
+                }
+                sb.Append(word);
+                curLineLength += word.Length;
+            }
+            return sb.ToString();
         }
 
         public override float Height() {
             return base.Height() * lines;
+        }
+
+        public override float LeftWidth() {
+            return 0f;
+        }
+
+        public override float RightWidth() {
+            return 0f;
         }
     }
 }
