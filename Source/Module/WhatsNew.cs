a394a19d4ff26639ef3ce7246e227a7b657549b3
Lozen <865466388@qq.com>
2025-11-23 00:56:45 +0800

    v2.2.8 release

--- Source/Module/WhatsNew.cs
+++ Source/Module/WhatsNew.cs
@@ -103,6 +103,7 @@ public static class WhatsNew {
         AddLog("2.2.5", "Bugfix: Fix a NullReferenceException. (thanks @Michal 338)", "Feature: AutoWatch supports CrumblePlatform.");
         AddLog("2.2.6", "Feature: AutoWatch support MaxHelpingHand's CustomizableCrumblePlatform.");
         AddLog("2.2.7", "Bugfix: Support latest Chronia Helper update.");
+        AddLog("2.2.8", "Bugfix: Make OoO Stepper work again.");
         UpdateLogs.Sort((x, y) => new Version(y.Item1).CompareTo(new Version(x.Item1)));
     }
 
